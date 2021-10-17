5ed42177e21593d1d97d8645c03ab81023692eba
AlexStash666 <alexanderstashinski@gmail.com>
2021-10-17 21:55:54 +0300

    Serializer. Viewset. router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [people, setPeople] = useState([])
+
+  useEffect(()=>{
+    axios({
+      method: "GET",
+      url: "http://127.0.0.1:8000/api/test-api/"
+    }).then(response => {
+      setPeople(response.data)
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {people.map(p => (
+          <li key={p.id}>{p.name}</li>
+      ))}
+    </ul>
     </div>
   );
 }
