48c53ce4cd6585cb7965fc4aa2603fe54a5474db
AlexStash666 <alexanderstashinski@gmail.com>
2021-10-17 23:59:09 +0300

    Refactor+Boot4

--- src/App.js
+++ src/App.js
@@ -5,23 +5,53 @@ import './App.css';
 
 function App() {
 
-  const [people, setPeople] = useState([])
-
-  useEffect(()=>{
-    axios({
-      method: "GET",
-      url: "http://127.0.0.1:8000/api/test-api/"
-    }).then(response => {
-      setPeople(response.data)
-    })
-  }, [])
   return (
+
     <div className="App">
-    <ul>
-      {people.map(p => (
-          <li key={p.id}>{p.name}</li>
-      ))}
-    </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                   role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr className="dropdown-divider"></hr>
+                  </li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search"
+                     aria-label="Search"></input>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    <h1>Hello from React to Django</h1>
     </div>
   );
 }
